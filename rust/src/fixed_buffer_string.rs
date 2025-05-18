ed09c14253237701dd1dc53a0dc1c91c6881643a
Robert R. Russell <robert@rrbrussell.com>
2025-05-18 17:51:47 -0500

    Implementing index and Eq PartialEq traits.

--- rust/src/fixed_buffer_string.rs
+++ rust/src/fixed_buffer_string.rs
@@ -3,7 +3,10 @@ The fixed_buffer_string is a nice tool for dealing with shorter string that
 avoid most of the reallocation issues. These are ASCII only.
  */
 
+use std::cmp::PartialEq;
 use std::convert::TryFrom;
+use std::ops::Add;
+use std::ops::Index;
 
 pub struct FBS {
     start: usize,
@@ -43,6 +46,36 @@ impl TryFrom<&str> for FBS {
     }
 }
 
+impl PartialEq for FBS {
+    fn eq(&self, other: &Self) -> bool {
+        if self.len() == other.len() {
+            for i in (0..self.len()) {
+                if self[i] != other[i] {
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+impl Eq for FBS {}
+
+impl Index<usize> for FBS {
+    type Output = u8;
+
+    fn index(&self, ith: usize) -> &Self::Output {
+        if ith >= self.len() {
+            panic!("{} is outside of the range (0..{}).",
+                ith, self.len());
+        }
+        return &(self.data[self.start + ith]);
+    }
+}
+
+
 /**
  */
 impl FBS {
@@ -54,10 +87,21 @@ impl FBS {
     }
 
     /**
+    The non panicking version of the index operator.
+    */
+    pub fn get(&self, index: usize) -> Result<&u8, &'static str> {
+        if index >= self.len() {
+            return Err("Index is out of bounds.");
+        } else {
+            return Ok(&self.data[self.start.add(index)]);
+        }
+    }
+
+    /**
     How much extra room is available for expanding the rear of the string.
     */
     pub fn free_capactiy(&self) -> usize {
-        return DATA_SIZE - self.end - self.start;
+        return DATA_SIZE - self.end;
     }
 
     /**
@@ -83,6 +127,20 @@ impl FBS {
         return FBS { start: 0, end: 0, data: [0; DATA_SIZE] };
     }
 
+    /**
+    Insert an &str into the FBS. Returns an Option<usize> with the number of
+    inserted values. None means an error other than just the entire &str did not
+    fit into the FBS. Usually this means the input either starts with a
+    non-ASCII character or is completely empty.
+     */
+    // #[must_use]
+    // pub fn insert(&mut self, new_data: &str) -> Option<usize> {
+    //     if new_data.is_empty() || !new_data[0].is_ascii() {
+    //         return None;
+    //     }
+    //     for (0..self)
+    // }
+    
     /**
     This is only used for testing.
     */
@@ -110,5 +168,17 @@ mod test {
         assert!(subject.start() == 0);
         assert!(subject.end() == 12);
         assert_eq!(subject.len(), "GoodBye 007.".len());
+    }
+
+    #[test]
+    pub fn test_free_capacity() {
+        let mut subject: FBS = FBS::new();
+        // An empty FBS should have a capacity of DATA_SIZE bytes.
+        assert_eq!(subject.free_capactiy(), DATA_SIZE);
+    }
+
+    #[test]
+    pub fn test_logic_free_capacity() {
+        fn 
     }
 }
